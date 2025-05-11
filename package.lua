dd162d957c5e731fbe755207145c8f3b6c05f3d7
Richy Z <64844585+Richy-Z@users.noreply.github.com>
2025-05-11 14:37:52 +0100

    Add package and vscode settings

---
+++ package.lua
@@ -0,0 +1,14 @@
+return {
+    name = "Richy-Z/input",
+    version = "0.0.1",
+    description = "Cross-platform keyboard and mouse library for Luvit",
+    tags = { "keyboard", "mouse", "input", "press", "touch", "cross-platform" },
+    license = "Apache 2.0",
+    author = { name = "Richy Z", email = "64844585+Richy-Z@users.noreply.github.com" },
+    homepage = "https://github.com/Richy-Z/input",
+    dependencies = {},
+    files = {
+        "**.lua",
+        "!test*"
+    }
+}
